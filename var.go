6ad7b041d72765a8aa037473ab4f8ef6f25cd704
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-26 23:57:05 +0530

    go-lang

---
+++ var.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var greeting string = "ranjan"
+	fmt.Println(greeting)
+
+	var str string
+	var intr int
+	fmt.Print("value daal")
+	fmt.Scanf("%s %t", &str&intr)
+	fmt.Println(str, intr)
+}
